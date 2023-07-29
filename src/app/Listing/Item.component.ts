c73c2e24502f9ad550b474900b97f290524e69ff
sooraj <soorajnr4@gmail.com>
2023-07-29 14:20:27 +0530

    item component seperate

--- src/app/Listing/Item.component.ts
+++ src/app/Listing/Item.component.ts
@@ -12,26 +12,9 @@ import { MaterialModule } from '../material.module';
     RouterModule,
     MaterialModule
   ],
-  template: `
-  
-                <mat-card class="example-card">
-                  <!-- <p class="flag">{{ListtingItems.Negotiable}}</p>                  -->
-                  <!-- <div mat-card-image class="bg-img-1" [ngStyle]="{ 'background-image': 'url(' + ListtingItems.photo + ')' }"></div> -->
-
-                  <div mat-card-image class="bg-img-1" style="background: url({{ListtingItems.photo}});"></div>              
-                  <mat-card-content class="mx-2">
-                    <h2 class="mb-1 mt-3 text-truncate">{{ListtingItems.name}}</h2>
-                    <!-- <p class="text-truncate">{{ ListtingItems.city}}, {{ListtingItems.state }}</p> -->
-                  </mat-card-content>
-                  <mat-card-actions class="m-3 d-flex justify-content-between">
-                    <button mat-raised-button color="primary" [routerLink]="['/details', ListtingItems.id]">Buy Now</button>
-                    <button mat-button color="warn"><h2 class="mb-0">Rs. {{ListtingItems.price}}</h2></button>
-                  </mat-card-actions>
-                </mat-card>
-             
-  
-  `,
+  templateUrl: './Item.component.html',
   styleUrls: ['./Item.component.scss'],
+  
 })
 
 export class ListtingItemsComponent {
